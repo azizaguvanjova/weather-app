d58cf575f7213151d5c886bade1ad4903354e93c
azizaguvanjova <aziza.guvanjova13@gmail.com>
2024-08-24 21:17:38 +0300

    updated project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {  useState } from "react"
+import WeatherInfo from "./WeatherInfo"
+import Header from "./Header"
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [data,setData] =useState({})
+  const [location,setLocation] =useState("")
 
-export default App;
+  const API_KEY= "0d05a9bb20bff8845a78cafde4ae705d"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY}`
+
+
+  
+ const searchLocation = (e)=>{
+    if(e.key === 'Enter'){
+      fetch(url)
+      .then(response => response.json())
+      .then(data => setData(data))
+      // console.log(data)
+      .catch(error => console.error(error))
+      setLocation("")
+    }
+    
+  }
+  return (
+    <>
+    <Header/>
+    <div className="w-full h-full relative">
+      <div className="text-center p-4">
+        <input type="text" className="py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 placeholder:text-gray-400
+         focus:outline-none bg-white-600/100 shadow-md"
+        placeholder="şehir adi girin"
+        value={location}
+        onChange={(e) =>setLocation(e.target.value)}
+        onKeyDown={searchLocation}
+        />
+
+      </div>
+      <WeatherInfo weatherData={data} />
+    </div>
+    </>
+  )
+}
+export default App
---
+++ src/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <div >
+        <h1 className="flex justify-center items-center ">Project:8 Weather App</h1>
+    </div>
+  )
+}
+export default Header
